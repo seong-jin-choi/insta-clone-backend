535af966434e8f08c182de7e64c29b1798356db3
CODESPACE THOR <thor@codespace.im>
2023-12-08 22:33:46 +0900

    프리즈마 추가

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
-
 const resolvers = {
   Query: {
-    hello: () => "hello world",
+    movies: () => [],
+    movie: () => ({ title: "hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: () => "",
   },
 };
 
