99c7067af72f4afbbf19995418c0283fa997832e
412 <85489519+laybacksound96@users.noreply.github.com>
2023-12-13 22:35:35 +0900

    계정 추가

--- src/schema.ts
+++ src/schema.ts
@@ -2,8 +2,8 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.ts`);
 
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,5 +1,14 @@
+import client from "../client";
+import bcrypt from "bcrypt";
 export default {
   Mutation: {
-    createAccount: (_, { firstName, lastName, username, email, password }) => {},
+    createAccount: async (_, { firstName, lastName, username, email, password }) => {
+      const existingUser = await client.user.findFirst({ where: { OR: [{ username }, { email }] } });
+      console.log(existingUser);
+
+      const HashedPassword = await bcrypt.hash(password, 10);
+
+      return client.user.create({ data: { firstName, lastName, username, email, password: HashedPassword } });
+    },
   },
 };
