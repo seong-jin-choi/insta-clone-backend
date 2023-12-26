e076b69d685fe549430e420821ec63b4ba6ffcbd
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2023-12-26 16:10:45 +0900

    validation and error handling

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,9 @@ import "dotenv/config";
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
 });
-
-const PORT = process.env.PORT;
 
 server.listen(PORT).then(() => console.log(`server is running on port ${PORT}`));
--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -3,12 +3,17 @@ import bcrypt from "bcrypt";
 export default {
   Mutation: {
     createAccount: async (_, { firstName, lastName, username, email, password }) => {
-      const existingUser = await client.user.findFirst({ where: { OR: [{ username }, { email }] } });
-      console.log(existingUser);
+      try {
+        const existingUser = await client.user.findFirst({ where: { OR: [{ username }, { email }] } });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const HashedPassword = await bcrypt.hash(password, 10);
 
-      const HashedPassword = await bcrypt.hash(password, 10);
-
-      return client.user.create({ data: { firstName, lastName, username, email, password: HashedPassword } });
+        return client.user.create({ data: { firstName, lastName, username, email, password: HashedPassword } });
+      } catch (error) {
+        return error;
+      }
     },
   },
 };
